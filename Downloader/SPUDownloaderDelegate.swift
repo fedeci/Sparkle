750fa9b3c4074a0f51eee3e0336899a37e813b6f
Federico Ciardi <fed.ciardi@gmail.com>
2020-07-26 20:32:02 +0200

    docs: add SPUDownloaderDelegate and SPUDownloaderProtocol comments
    Remove SPUDownloaderDelegate.h and SPUDownloaderProtocol.h

--- Downloader/SPUDownloaderDelegate.swift
+++ Downloader/SPUDownloaderDelegate.swift
@@ -11,12 +11,19 @@ import Foundation
 @objc
 protocol SPUDownloaderDelegate: NSObjectProtocol {
     
+    // This is only invoked for persistent downloads
     func downloaderDidSetDestinationName(_ destinationName: String?, temporaryDirection: String?)
     
-    func downloaderDidReceiveExpectedContentLength(_ expectedContentLength: __int64_t)
+    // Under rare cases, this may be called more than once, in which case the current progress should be reset back to 0
+    // This is only invoked for persistent downloads
+    func downloaderDidReceiveExpectedContentLength(_ expectedContentLength: Int64)
     
-    func downloaderDidReceiveData(ofLength length: __uint64_t)
+    // This is only invoked for persistent downloads
+    func downloaderDidReceiveDataOfLength(_ length: UInt64)
     
-    func downloaderDidFail(withError error: NSError)
+    // downloadData is nil if this is a persisent download, otherwise it's non-nil if it's a temporary download
+    func downloaderDidFinishWithTemporaryDownloadData(_ downloadData: SPUDownloadData?)
+    
+    func downloaderDidFailWithError(_ error: NSError)
 
 }
