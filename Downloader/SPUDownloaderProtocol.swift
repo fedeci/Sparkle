c2774fdc7cd963f9decebadda0b94e37fa014b8d
Federico Ciardi <fed.ciardi@gmail.com>
2020-07-26 13:15:04 +0200

    refactor: add SPUDownloaderProtocol.swift

---
+++ Downloader/SPUDownloaderProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  SPUDownloaderProtocol.swift
+//  SparkleDownloader
+//
+//  Created by Federico Ciardi on 26/07/2020.
+//  Copyright © 2020 Sparkle Project. All rights reserved.
+//
+
+import Foundation
+
+protocol SPUDownloaderProtocol {
+    func startPersistentDownloadWithRequest(_ request: SPUURLRequest, bundleIdentifier: String, desiredFilename: String)
+    
+    func startTemporaryDownloadWithRequest(_ request: SPUURLRequest)
+    
+    func cancelDownload()
+}
+
