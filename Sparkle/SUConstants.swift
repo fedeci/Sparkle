b0f90983b2612a16116aa8b4d55402a4aa0cdbf1
Federico Ciardi <fed.ciardi@gmail.com>
2020-07-26 20:36:26 +0200

    refactor: add SUConstants.swift and SUErrors.swift

---
+++ Sparkle/SUConstants.swift
@@ -0,0 +1,85 @@
+//
+//  SUConstants.swift
+//  Sparkle
+//
+//  Created by Federico Ciardi on 26/07/2020.
+//  Copyright © 2020 Sparkle Project. All rights reserved.
+//
+
+import Foundation
+
+#if !DEBUG
+let DEBUG = false
+#endif
+
+// Define some minimum intervals to avoid DoS-like checking attacks
+let SUMinimumUpdateCheckInterval: TimeInterval = DEBUG ? 60 : (60 * 60)
+let SUDefaultUpdateCheckInterval: TimeInterval = DEBUG ? 60 : (60 * 60 * 24)
+
+// If the update has already been automatically downloaded, we normally don't want to bug the user about the update
+// However if the user has gone a very long time without quitting an application, we will bug them
+// This is the time interval for a "week" it doesn't matter that this measure is imprecise.
+let SUImpatientUpdateCheckInterval: TimeInterval = DEBUG ? (60 * 2) : (60 * 60 * 24 * 7)
+
+
+let SUBundleIdentifier = Bundle.main.object(forInfoDictionaryKey: "SPARKLE_BUNDLE_IDENTIFIER") as? String ?? ""
+
+let SUAppcastAttributeValueMacOS = "macos"
+
+let SUTechnicalErrorInformationKey = "SUTechnicalErrorInformation"
+
+let SUFeedURLKey = "SUFeedURL"
+let SUHasLaunchedBeforeKey = "SUHasLaunchedBefore"
+let SUUpdateRelaunchingMarkerKey = "SUUpdateRelaunchingMarker"
+let SUShowReleaseNotesKey = "SUShowReleaseNotes"
+let SUSkippedVersionKey = "SUSkippedVersion"
+let SUScheduledCheckIntervalKey = "SUScheduledCheckInterval"
+let SULastCheckTimeKey = "SULastCheckTime"
+let SUExpectsDSASignatureKey = "SUExpectsDSASignature"
+let SUExpectsEDSignatureKey = "SUExpectsEDSignatureKey"
+let SUPublicDSAKeyKey = "SUPublicDSAKey"
+let SUPublicDSAKeyFileKey = "SUPublicDSAKeyFile"
+let SUPublicEDKeyKey = "SUPublicEDKey"
+let SUAutomaticallyUpdateKey = "SUAutomaticallyUpdate"
+let SUAllowsAutomaticUpdatesKey = "SUAllowsAutomaticUpdates"
+let SUEnableSystemProfilingKey = "SUEnableSystemProfiling"
+let SUEnableAutomaticChecksKey = "SUEnableAutomaticChecks"
+let SUSendProfileInfoKey = "SUSendProfileInfo"
+let SULastProfileSubmitDateKey = "SULastProfileSubmissionDate"
+let SUPromptUserOnFirstLaunchKey = "SUPromptUserOnFirstLaunch"
+let SUEnableJavaScriptKey = "SUEnableJavaScript"
+let SUFixedHTMLDisplaySizeKey = "SUFixedHTMLDisplaySize"
+let SUDefaultsDomainKey = "SUDefaultsDomain"
+/**
+ * Error domain used by Sparkle
+ */
+let SUSparkleErrorDomain = "SUSparkleErrorDomain"
+
+let SUAppendVersionNumberKey = "SUAppendVersionNumber"
+let SUEnableAutomatedDowngradesKey = "SUEnableAutomatedDowngrades"
+let SUNormalizeInstalledApplicationNameKey = "SUNormalizeInstalledApplicationName"
+let SURelaunchToolNameKey = "SURelaunchToolName"
+
+let SUAppcastAttributeDeltaFrom = "sparkle:deltaFrom"
+let SUAppcastAttributeDSASignature = "sparkle:dsaSignature"
+let SUAppcastAttributeEDSignature = "sparkle:edSignature"
+let SUAppcastAttributeShortVersionString = "sparkle:shortVersionString"
+let SUAppcastAttributeVersion = "sparkle:version"
+let SUAppcastAttributeOsType = "sparkle:os"
+let SUAppcastAttributeInstallationType = "sparkle:installationType"
+
+let SUAppcastElementCriticalUpdate = "sparkle:criticalUpdate"
+let SUAppcastElementDeltas = "sparkle:deltas"
+let SUAppcastElementMinimumSystemVersion = "sparkle:minimumSystemVersion"
+let SUAppcastElementMaximumSystemVersion = "sparkle:maximumSystemVersion"
+let SUAppcastElementReleaseNotesLink = "sparkle:releaseNotesLink"
+let SUAppcastElementTags = "sparkle:tags"
+
+let SURSSAttributeURL = "url"
+let SURSSAttributeLength = "length"
+
+let SURSSElementDescription = "description"
+let SURSSElementEnclosure = "enclosure"
+let SURSSElementLink = "link"
+let SURSSElementPubDate = "pubDate"
+let SURSSElementTitle = "title"
---
+++ Sparkle/SUErrors.swift
@@ -0,0 +1,51 @@
+//
+//  SUErrors.swift
+//  Sparkle
+//
+//  Created by Federico Ciardi on 26/07/2020.
+//  Copyright © 2020 Sparkle Project. All rights reserved.
+//
+
+import Foundation
+
+enum SUError: OSStatus {
+    // Configuration phase errors
+    case SUNoPublicDSAFoundError = 0001
+    case SUInsufficientSigningError = 0002
+    case SUInsecureFeedURLError = 0003
+    case SUInvalidFeedURLError = 0004
+    case SUInvalidUpdaterError = 0005
+    case SUInvalidHostBundleIdentifierError = 0006
+    case SUInvalidHostVersionError = 0007
+    
+    // Appcast phase errors.
+    case SUAppcastParseError = 1000
+    case SUNoUpdateError = 1001
+    case SUAppcastError = 1002
+    case SURunningFromDiskImageError = 1003
+    case SUResumeAppcastError = 1004
+    case SURunningTranslocated = 1005
+    
+    // Download phase errors.
+    case SUTemporaryDirectoryError = 2000
+    case SUDownloadError = 2001
+    
+    // Extraction phase errors.
+    case SUUnarchivingError = 3000
+    case SUSignatureError = 3001
+    
+    // Installation phase errors.
+    case SUFileCopyFailure = 4000
+    case SUAuthenticationFailure = 4001
+    case SUMissingUpdateError = 4002
+    case SUMissingInstallerToolError = 4003
+    case SURelaunchError = 4004
+    case SUInstallationError = 4005
+    case SUDowngradeError = 4006
+    case SUInstallationCanceledError = 4007
+    case SUInstallationAuthorizeLaterError = 4008
+    case SUNotAllowedInteractionError = 4009
+    
+    // API misuse errors.
+    case SUIncorrectAPIUsageError = 5000
+}
