571de95dd0f70b37e5e266bb8a93451695376e5f
Federico Ciardi <fed.ciardi@gmail.com>
2020-07-26 13:24:23 +0200

    refactor: add SPUDownloaderDelegate.swift

---
+++ Downloader/SPUDownloaderDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SPUDownloaderDelegate.swift
+//  SparkleDownloader
+//
+//  Created by Federico Ciardi on 26/07/2020.
+//  Copyright © 2020 Sparkle Project. All rights reserved.
+//
+
+import Foundation
+
+protocol SPUDownloaderDelegate: NSObjectProtocol {
+    
+    func downloaderDidSetDestinationName(_ destinationName: String, temporaryDirection: String)
+    
+    func downloaderDidReceiveExpectedContentLength(_ expectedContentLength: __int64_t)
+    
+    func downloaderDidReceiveData(ofLength length: __int64_t)
+    
+    func downloaderDidFail(withError error: NSError)
+
+}
