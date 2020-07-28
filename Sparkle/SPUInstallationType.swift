2742aa2e7169356d0f1cdee5a2313bcbc73302e5
Federico Ciardi <fed.ciardi@gmail.com>
2020-07-28 17:23:45 +0200

    refactor: add SPUInstallationType.swift

---
+++ Sparkle/SPUInstallationType.swift
@@ -0,0 +1,25 @@
+//
+//  SPUInstallationType.swift
+//  Sparkle
+//
+//  Created by Federico Ciardi on 26/07/2020.
+//  Copyright © 2020 Sparkle Project. All rights reserved.
+//
+
+import Foundation
+
+let SPUInstallationTypeApplication = "application" // the default installation type for ordinary application updates
+let SPUInstallationTypeGuidedPackage = "package" // the preferred installation type for package installations
+let SPUInstallationTypeInteractivePackage = "interactive-package" // the deprecated installation type; use guided package instead
+
+let SPUInstallationTypeDefault = SPUInstallationTypeApplication
+
+let SPUInstallationTypesArray = [
+    SPUInstallationTypeApplication,
+    SPUInstallationTypeGuidedPackage,
+    SPUInstallationTypeInteractivePackage
+]
+
+func SPUValidInstallationType(_ x: String) -> Bool {
+    return SPUInstallationTypesArray.contains(x)
+}
