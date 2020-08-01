d91bd67ea7ef8450eb2a4da24baaf0024cbdd776
Federico Ciardi <fed.ciardi@gmail.com>
2020-08-01 11:15:56 +0200

    refactor: add SUStatusInfoProtocol.swift

---
+++ InstallerStatus/SUStatusInfoProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  SUStatusInfoProtocol.swift
+//  Installer Progress
+//
+//  Created by Federico Ciardi on 01/08/2020.
+//  Copyright © 2020 Sparkle Project. All rights reserved.
+//
+
+import Foundation
+
+@objc
+protocol SUStatusInfoProtocol {
+    func probeStatusInfoWithReply(_ reply: @escaping (Data?) -> Void)
+    
+    func probeStatusConnectivityWithReply(_ reply: () -> Void)
+}
