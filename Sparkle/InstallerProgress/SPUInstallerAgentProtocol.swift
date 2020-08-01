90a995c06c57ec86e650d83b162766bd0bee00d7
Federico Ciardi <fed.ciardi@gmail.com>
2020-08-01 11:21:37 +0200

    refactor: add SPUInstallerAgentProtocol.swift

---
+++ Sparkle/InstallerProgress/SPUInstallerAgentProtocol.swift
@@ -0,0 +1,24 @@
+//
+//  SPUInstallerAgentProtocol.swift
+//  Installer Progress
+//
+//  Created by Federico Ciardi on 31/07/2020.
+//  Copyright © 2020 Sparkle Project. All rights reserved.
+//
+
+import Foundation
+
+@objc
+protocol SPUInstallerAgentProtocol {
+    func registerApplicationBundlePath(_ applicationBundlePath: String, reply: @escaping (NSNumber?) -> Void)
+    
+    func registerInstallationInfoData(_ installationInfoData: Data)
+    
+    func sendTerminationSignal()
+    
+    func showProgress()
+    
+    func stopProgress()
+    
+    func relaunchPath(_ requestedPathToRelaunch: String)
+}
