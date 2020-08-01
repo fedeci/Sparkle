85893acdef4ee8a2a059953abf0ce8aa75e07ee9
Federico Ciardi <fed.ciardi@gmail.com>
2020-08-01 16:18:23 +0200

    refactor: add SULocalizations.swift

---
+++ Sparkle/SULocalizations.swift
@@ -0,0 +1,14 @@
+//
+//  SULocalizations.swift
+//  Sparkle
+//
+//  Created by Federico Ciardi on 01/08/2020.
+//  Copyright © 2020 Sparkle Project. All rights reserved.
+//
+
+import Foundation
+
+func SULocalizedString(key: String, comment: String = "") -> String {
+    let bundle = Bundle(identifier: SPUSparkleBundleIdentifier) ?? Bundle.main
+    return NSLocalizedString(key, tableName: "Sparkle", bundle: bundle, comment: comment)
+}
