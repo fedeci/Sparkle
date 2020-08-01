c93425a3e85fe39e476c1dc3c68f7115d97f3696
Federico Ciardi <fed.ciardi@gmail.com>
2020-08-01 11:19:19 +0200

    refactor: add InstallerProgressDelegate.swift

---
+++ Sparkle/InstallerProgress/InstallerProgressDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  InstallerProgressDelegate.swift
+//  Installer Progress
+//
+//  Created by Federico Ciardi on 31/07/2020.
+//  Copyright © 2020 Sparkle Project. All rights reserved.
+//
+
+import Foundation
+
+@objc
+protocol InstallerProgressDelegate: NSObjectProtocol {
+    func installerProgressShouldDisplayWithHost(_ host: SUHost)
+    
+    func installerProgressShouldStop()
+}
