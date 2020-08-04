742d68367dbc754d3ccc9f7ad9b63b7ec61b7227
Federico Ciardi <fed.ciardi@gmail.com>
2020-08-04 12:20:06 +0200

    refactor: add SUVersionComparisonProtocol.swift

---
+++ Sparkle/SUVersionComparisonProtocol.swift
@@ -0,0 +1,22 @@
+//
+//  SUVersionComparisonProtocol.swift
+//  Sparkle
+//
+//  Created by Federico Ciardi on 03/08/2020.
+//  Copyright © 2020 Sparkle Project. All rights reserved.
+//
+
+import Foundation
+
+/**
+Provides version comparison facilities for Sparkle.
+*/
+@objc
+protocol SUVersionComparison {
+    /**
+    An abstract method to compare two version strings.
+
+    Should return OrderedAscending if b `>` a, NSOrderedDescending if b `<` a, and OrderedSame if they are equivalent.
+    */
+    func compareVersion(_ versionA: String, toVersion versionB: String) -> ComparisonResult // *** MAY BE CALLED ON NON-MAIN THREAD!
+}
