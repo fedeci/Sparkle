c8db269add5c114338b749a1bbb7662a83b767bc
Federico Ciardi <fed.ciardi@gmail.com>
2020-08-04 17:50:52 +0200

    refactor: add SUInstallerConnectionProtocol.swift

---
+++ InstallerConnection/SUInstallerConnectionProtocol.swift
@@ -0,0 +1,21 @@
+//
+//  SUInstallerConnectionProtocol.swift
+//  Sparkle
+//
+//  Created by Federico Ciardi on 04/08/2020.
+//  Copyright © 2020 Sparkle Project. All rights reserved.
+//
+
+import Foundation
+
+@objc
+protocol SUInstallerConnectionProtocol {
+    #warning("Inherit SUInstallerCommunicationProtocol?")
+    func handleMessageWithIdentifier(_ identifier: Int32, data: Data)
+    
+    func setInvalidationHandler(_ invalidationHandler: @escaping () -> Void)
+    
+    func setServiceName(_ serviceName: String, hostPath: String, installationType: String)
+    
+    func invalidate()
+}
